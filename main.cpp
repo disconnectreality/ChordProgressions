5aee8b636e5441761edddd1983e2bbc17b872659
disconnectality <drocks07@outlook.com>
2024-01-22 09:29:45 -0800

    update style and structure

--- main.cpp
+++ main.cpp
@@ -3,27 +3,34 @@
 #include <vector>
 using namespace std;
 
-string Notes[12] = {"G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G"};
+string Notes[12] = 
+{
+"G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", 
+"F#", "G"
+};
 
-void chord(int series[], int x, int y) {
-    int product;
+void chord(int series[], int firstShift, int secondShift) 
+{
+    int noteIndex;
 
-    for(int i=0;i<=11;i++) {
-        product = i;
-        cout << Notes[product] << endl;
-        product += x;
-        cout << Notes[product % 12] << endl;
-        product += y;
-        cout << Notes[product % 12] << endl;
+    for(int i=0;i<=11;i++) 
+    {
+        noteIndex = i;
+        cout << Notes[noteIndex] << endl;
+        noteIndex += firstShift;
+        cout << Notes[noteIndex % 12] << endl;
+        noteIndex += secondShift;
+        cout << Notes[noteIndex % 12] << endl;
         cout << "End of position " << series[i] << endl;
     }
 }
 
-int main() {
+int main() 
+{
     int series[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
-    char choice;
-    cin >> choice; 
-    switch (choice) {
+    char selection;
+    cin >> selection; 
+    switch (selection) {
         case 'm':
             chord(series,3,4);
             break;
